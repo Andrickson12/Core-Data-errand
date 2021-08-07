290d7709ebf366d5fe9100af24ad5db5486437ea
Andrickson12 <Andrickson12>
2021-08-07 00:06:07 -0400

    Initial Commit

---
+++ Core Data errand/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  Core Data errand
+//
+//  Created by Andrickson Coste on 8/6/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
